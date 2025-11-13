722543425bd9c191f604ee55b309d8135dcf5bbc
Shuhrat <abbosovich1999@gmail.com>
2025-11-13 22:19:09 +0500

    pos2t

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "50mb" }));
 
 // Cloudinary sozlamalari
 cloudinary.config({
